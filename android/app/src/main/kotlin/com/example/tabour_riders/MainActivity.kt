c0114f4015f10a1420a2f9ce782436a0103ce9b5
David Onuche <onuchedavid34@gmail.com>
2024-03-07 02:25:23 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/tabour_riders/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tabour_riders
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tabour_riders/global/global.dart';
+import 'package:tabour_riders/splash%20screen/spalsh_screen.dart';
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  sharedPreferences = await SharedPreferences.getInstance();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tabour Riders',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
